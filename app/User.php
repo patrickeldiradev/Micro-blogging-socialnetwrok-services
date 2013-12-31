bc5108766dd86d0a70ec3b235d8b332bcf82c1d0
Patrick <patrick.eldira.dev@gmail.com>
2014-01-01 00:00:00 +0500

    Change User model auth from passport to jwt

--- app/User.php
+++ app/User.php
@@ -4,13 +4,14 @@ namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Facades\Storage;
-use Laravel\Passport\HasApiTokens;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, Notifiable;
+    use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
@@ -38,6 +39,28 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+
+    // Rest omitted for brevity
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 
     /**
      * Set the user's password.
